7ff71e6cf7b570f42a6e249b7a162e41360da651
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-04-05 18:03:59 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import "./App.css";
-import Button from "@mui/material/Button";
-
 function App() {
-  return (
-    <>
-      <Button variant="contained">Hello world</Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
