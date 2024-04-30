65af5cc71aafa498f90c775d1e13cad6514c8a44
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-05-01 00:40:05 +0530

    Added Axios Interceptor

---
+++ src/hooks/useLocalStotage.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+	const storedValue =
+		typeof window !== "undefined" ? localStorage.getItem(key) : null;
+	const initial = storedValue ? JSON.parse(storedValue) : initialValue;
+
+	const [localStorageValue, setLocalStorageValue] = useState(initial);
+
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(localStorageValue));
+	}, [key, localStorageValue]);
+
+	return [localStorageValue, setLocalStorageValue];
+};
+
+export default useLocalStorage;
