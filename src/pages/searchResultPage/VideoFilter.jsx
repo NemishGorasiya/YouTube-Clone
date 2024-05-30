1b90be2651f985c71fe300b5767e4776097be7fd
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-05-31 01:35:59 +0530

    Added filter fields

--- src/pages/searchResultPage/VideoFilter.jsx
+++ src/pages/searchResultPage/VideoFilter.jsx
@@ -1,33 +1,82 @@
-import { Button, styled } from "@mui/material";
+import { Box, Button, Typography, styled } from "@mui/material";
+import MuiDivider from "@mui/material/Divider";
 import TuneIcon from "@mui/icons-material/Tune";
 import { useState } from "react";
 import StyledModal from "../../components/StyledModal";
+import { filterFields } from "../../utils/constant";
 
 const FiltersButton = styled(Button)(() => ({
-  "&:hover": {
-    background: "rgba(255, 255, 255, 0.2)",
-  },
+	"&:hover": {
+		background: "rgba(255, 255, 255, 0.2)",
+	},
+}));
+const FilterFieldWrapper = styled(Box)(() => ({
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "flex-start",
+	textAlign: "start",
+	gap: "8px",
+}));
+const FilterContentWrapper = styled(Box)(() => ({
+	display: "flex",
+	gap: "16px",
+}));
+
+const Divider = styled(MuiDivider)(() => ({
+	borderBottomWidth: "2px",
+}));
+const FilterFieldTypography = styled(Typography)(() => ({
+	color: "grey",
+	cursor: "pointer",
 }));
 
 const VideoFilter = () => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
-  return (
-    <>
-      <FiltersButton
-        onClick={handleOpen}
-        variant="text"
-        startIcon={<TuneIcon />}
-      >
-        Filters
-      </FiltersButton>
-      <StyledModal open={open} handleClose={handleClose}>
-        hello
-      </StyledModal>
-    </>
-  );
+	const [filters, setFilters] = useState({});
+
+	const handleFilterFieldClick = ({ filterKey, filterValue }) => {
+		setFilters((prevFilters) => ({
+			...prevFilters,
+			[filterKey]: filterValue,
+		}));
+	};
+
+	console.log(filters, "filters");
+
+	return (
+		<>
+			<FiltersButton onClick={handleOpen} variant="text" endIcon={<TuneIcon />}>
+				Filters
+			</FiltersButton>
+			<StyledModal open={open} handleClose={handleClose}>
+				<FilterContentWrapper>
+					{filterFields.map((filter) => (
+						<FilterFieldWrapper key={filter.title}>
+							<Typography variant="body2">{filter.title}</Typography>
+							<Divider flexItem />
+							{filter.fields.map((field, idx) => (
+								<FilterFieldTypography
+									onClick={() =>
+										handleFilterFieldClick({
+											filterKey: filter.filterKey,
+											filterValue: field.value,
+										})
+									}
+									key={idx}
+									variant="body2"
+								>
+									{field.label}
+								</FilterFieldTypography>
+							))}
+						</FilterFieldWrapper>
+					))}
+				</FilterContentWrapper>
+			</StyledModal>
+		</>
+	);
 };
 
 export default VideoFilter;
--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -13,77 +13,186 @@ import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
 
 export const SideBarLinks = [
-  {
-    title: "",
-    links: [
-      {
-        icon: <HomeOutlinedIcon />,
-        filledIcon: <HomeIcon />,
-        outlinedIcon: <HomeOutlinedIcon />,
-        label: "Home",
-        link: "/",
-      },
-    ],
-  },
-  {
-    title: "You ",
-    isProtected: true,
-    titleIcon: <KeyboardArrowRightIcon />,
-    links: [
-      {
-        icon: <PlaylistPlayIcon />,
-        filledIcon: <PlaylistPlayIcon />,
-        outlinedIcon: <PlaylistPlayIcon />,
-        label: "Playlists",
-        link: "/feed/playlists",
-      },
-      {
-        icon: <ThumbUpOutlinedIcon />,
-        filledIcon: <ThumbUpIcon />,
-        outlinedIcon: <ThumbUpOutlinedIcon />,
-        label: "Liked videos",
-        link: "/playlist?list=LL",
-        queryKey: "list",
-        queryValue: "LL",
-      },
-      {
-        icon: <SubscriptionsOutlinedIcon />,
-        filledIcon: <SubscriptionsIcon />,
-        outlinedIcon: <SubscriptionsOutlinedIcon />,
-        label: "Subscriptions",
-        link: "/feed/channels",
-      },
-    ],
-  },
+	{
+		title: "",
+		links: [
+			{
+				icon: <HomeOutlinedIcon />,
+				filledIcon: <HomeIcon />,
+				outlinedIcon: <HomeOutlinedIcon />,
+				label: "Home",
+				link: "/",
+			},
+		],
+	},
+	{
+		title: "You ",
+		isProtected: true,
+		titleIcon: <KeyboardArrowRightIcon />,
+		links: [
+			{
+				icon: <PlaylistPlayIcon />,
+				filledIcon: <PlaylistPlayIcon />,
+				outlinedIcon: <PlaylistPlayIcon />,
+				label: "Playlists",
+				link: "/feed/playlists",
+			},
+			{
+				icon: <ThumbUpOutlinedIcon />,
+				filledIcon: <ThumbUpIcon />,
+				outlinedIcon: <ThumbUpOutlinedIcon />,
+				label: "Liked videos",
+				link: "/playlist?list=LL",
+				queryKey: "list",
+				queryValue: "LL",
+			},
+			{
+				icon: <SubscriptionsOutlinedIcon />,
+				filledIcon: <SubscriptionsIcon />,
+				outlinedIcon: <SubscriptionsOutlinedIcon />,
+				label: "Subscriptions",
+				link: "/feed/channels",
+			},
+		],
+	},
 ];
 
 export const subscriptionStatusList = [
-  {
-    icon: <NotificationsNoneIcon />,
-    label: "Subscribed",
-  },
-  {
-    icon: <NotificationsOffIcon />,
-    label: "Disabled",
-  },
-  {
-    icon: <PersonRemoveIcon />,
-    label: "Unsubscribe",
-  },
+	{
+		icon: <NotificationsNoneIcon />,
+		label: "Subscribed",
+	},
+	{
+		icon: <NotificationsOffIcon />,
+		label: "Disabled",
+	},
+	{
+		icon: <PersonRemoveIcon />,
+		label: "Unsubscribe",
+	},
 ];
 
 export const privacyOptions = {
-  public: {
-    label: "Public",
-    description: "Anyone can search for and view",
-    icon: <PublicOutlinedIcon />,
-  },
-  private: {
-    label: "Private",
-    description: "Only you can view",
-    icon: <LockOutlinedIcon />,
-  },
+	public: {
+		label: "Public",
+		description: "Anyone can search for and view",
+		icon: <PublicOutlinedIcon />,
+	},
+	private: {
+		label: "Private",
+		description: "Only you can view",
+		icon: <LockOutlinedIcon />,
+	},
 };
 
 export const commentsAreOffGooglePageLink =
-  "https://support.google.com/youtube/answer/9706180?hl=en";
+	"https://support.google.com/youtube/answer/9706180?hl=en";
+
+export const filterFields = [
+	{
+		title: "SORT BY",
+		filterKey: "order",
+		fields: [
+			{
+				label: "Relevance",
+				value: "relevance",
+			},
+			{
+				label: "Upload date",
+				value: "date",
+			},
+			{
+				label: "View count",
+				value: "viewCount",
+			},
+			{
+				label: "Rating",
+				value: "rating",
+			},
+			{
+				label: "By title",
+				value: "title",
+			},
+			{
+				label: "Video count",
+				value: "videoCount",
+			},
+		],
+	},
+	{
+		title: "DIMENSIONS",
+		filterKey: "videoDimension",
+		fields: [
+			{
+				label: "2D",
+				value: "2d",
+			},
+			{
+				label: "3D",
+				value: "3d",
+			},
+		],
+	},
+	{
+		title: "DURATION",
+		filterKey: "videoDuration",
+		fields: [
+			{
+				label: "Under 4 minutes",
+				value: "short",
+			},
+			{
+				label: "4 - 20 minutes",
+				value: "medium",
+			},
+			{
+				label: "Over 20 minutes",
+				value: "long",
+			},
+		],
+	},
+	{
+		title: "EVENT TYPE",
+		filterKey: "eventType",
+		fields: [
+			{
+				label: "Completed",
+				value: "completed",
+			},
+			{
+				label: "live",
+				value: "live",
+			},
+			{
+				label: "Upcoming",
+				value: "upcoming",
+			},
+		],
+	},
+	{
+		title: "UPLOAD DATE",
+		filterKey: "publishedBefore",
+		fields: [
+			{
+				label: "Last hour",
+				value: "",
+			},
+			{
+				label: "Today",
+				value: "",
+			},
+			{
+				label: "This week",
+				value: "",
+			},
+			{
+				label: "This month",
+				value: "",
+			},
+			{
+				label: "This year",
+				value: "",
+			},
+		],
+	},
+];
