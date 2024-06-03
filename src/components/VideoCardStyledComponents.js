3e49f8290970c213414a0044935c7ab47159a33a
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-04 01:26:34 +0530

    Added card hover effect

--- src/components/VideoCardStyledComponents.js
+++ src/components/VideoCardStyledComponents.js
@@ -6,109 +6,133 @@ import MuiCardMedia from "@mui/material/CardMedia";
 import MuiTypography from "@mui/material/Typography";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import LiveTvIcon from "@mui/icons-material/LiveTv";
-import MuiCardActionArea from "@mui/material/CardActionArea";
 
 export const Card = styled(MuiCard)(({ theme }) => ({
-  background: theme.palette.background.default,
-  color: theme.palette.primary.main,
+	background: theme.palette.background.default,
+	color: theme.palette.primary.main,
+	position: "relative",
+	overflow: "visible",
+	cursor: "pointer",
+	height: "100%",
+	"&::before": {
+		content: `""`,
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		background: "rgba(255,255,255,0.1)",
+		transition: "transform 0.3s ease",
+		zIndex: 0,
+		display: "none",
+	},
+	"&:hover::before": {
+		transform: "scale(1.04)",
+		borderRadius: "16px",
+		display: "block",
+	},
+	"& > *": {
+		position: "relative",
+		zIndex: 1,
+	},
 }));
 
 export const CardMedia = styled(MuiCardMedia)(() => ({
-  borderRadius: "14px",
-  aspectRatio: "25/14",
-  height: "auto",
+	borderRadius: "14px",
+	aspectRatio: "25/14",
+	height: "auto",
 }));
 
 export const CardMediaWrapper = styled(MuiBox)(() => ({
-  position: "relative",
-  width: "100%",
+	position: "relative",
+	width: "100%",
 }));
 
 export const VideoIsLiveIndicator = styled("span")(() => ({
-  background: "red",
-  fontSize: "12px",
-  borderRadius: "2px",
-  color: "#FFFFFF",
-  marginRight: "4px",
-  padding: "2px 4px",
-  display: "inline-block",
+	background: "red",
+	fontSize: "12px",
+	borderRadius: "2px",
+	color: "#FFFFFF",
+	marginRight: "4px",
+	padding: "2px 4px",
+	display: "inline-block",
 }));
 
 export const LiveTvStyledIcon = styled(LiveTvIcon)(() => ({
-  fontSize: "12px",
-  marginRight: "4px",
+	fontSize: "12px",
+	marginRight: "4px",
 }));
 
 export const VideoDurationBadge = styled(MuiBox)(() => ({
-  position: "absolute",
-  bottom: "8px",
-  right: "12px",
-  display: "flex",
-  alignItems: "center",
-  fontSize: "13px",
-  background: "rgba(0,0,0,0.6)",
-  fontWeight: "600",
-  padding: "4px",
-  borderRadius: "4px",
-  color: "#fff",
+	position: "absolute",
+	bottom: "8px",
+	right: "12px",
+	display: "flex",
+	alignItems: "center",
+	fontSize: "13px",
+	background: "rgba(0,0,0,0.6)",
+	fontWeight: "600",
+	padding: "4px",
+	borderRadius: "4px",
+	color: "#fff",
 }));
 
 export const CardContent = styled(MuiCardContent)(() => ({
-  padding: "0",
-  display: "flex",
-  gap: "8px",
-  minWidth: "50%",
-  alignItems: "start",
-  flex: "1",
-  width: "100%",
+	padding: "0",
+	display: "flex",
+	gap: "8px",
+	minWidth: "50%",
+	alignItems: "start",
+	flex: "1",
+	width: "100%",
 }));
 
 export const ChannelThumbnail = styled("img")(() => ({
-  height: "36px",
-  width: "36px",
-  borderRadius: "50%",
+	height: "36px",
+	width: "36px",
+	borderRadius: "50%",
 }));
 
-export const CardActionArea = styled(MuiCardActionArea)(({ isListView }) => ({
-  display: "flex",
-  ...(isListView
-    ? {
-        gap: "16px",
-        alignItems: "start",
-      }
-    : {
-        gap: "8px",
-        flexDirection: "column",
-      }),
+export const CardActionArea = styled(MuiBox)(({ isListView }) => ({
+	display: "flex",
+	...(isListView
+		? {
+				gap: "16px",
+				alignItems: "start",
+		  }
+		: {
+				gap: "8px",
+				flexDirection: "column",
+		  }),
 }));
 
 export const VideoTitle = styled(MuiTypography)(() => ({
-  lineHeight: "1.5",
-  fontSize: "16px",
-  fontWeight: "500",
-  overflow: "hidden",
-  textOverflow: "ellipsis",
-  display: "-webkit-box",
-  WebkitBoxOrient: "vertical",
-  WebkitLineClamp: "2",
+	lineHeight: "1.5",
+	fontSize: "16px",
+	fontWeight: "500",
+	overflow: "hidden",
+	textOverflow: "ellipsis",
+	display: "-webkit-box",
+	WebkitBoxOrient: "vertical",
+	WebkitLineClamp: "2",
 }));
 
 export const ChannelName = styled(MuiTypography)(() => ({
-  display: "flex",
-  alignItems: "center",
-  fontSize: "14px",
+	display: "flex",
+	alignItems: "center",
+	fontSize: "14px",
 }));
 
 export const VideoMetadata = styled(MuiBox)(({ theme }) => ({
-  color: theme.palette.primary.light,
-  fontSize: "14px",
+	color: theme.palette.primary.light,
+	fontSize: "14px",
 }));
 
 export const VideoDetail = styled(MuiBox)(() => ({
-  flex: 1,
-  overflow: "hidden",
+	flex: 1,
+	overflow: "hidden",
 }));
 
 export const CheckCircleStyledIcon = styled(CheckCircleIcon)(() => ({
-  marginLeft: "5px",
+	marginLeft: "5px",
 }));
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,23 +3,26 @@ import { Outlet } from "react-router-dom";
 import AppBar from "../components/appBar/AppBar";
 import NoInternetPage from "../pages/noInternetPage/NoInternetPage";
 import {
-  DrawerHeader,
-  LayoutComponent,
-  RightPanel,
+	DrawerHeader,
+	LayoutComponent,
+	OutletWrapper,
+	RightPanel,
 } from "./LayoutStyledComponent";
 
 const Layout = () => {
-  return (
-    <LayoutComponent>
-      <AppBar />
-      <RightPanel component="main">
-        <DrawerHeader />
-        <NoInternetPage>
-          <Outlet />
-        </NoInternetPage>
-      </RightPanel>
-    </LayoutComponent>
-  );
+	return (
+		<LayoutComponent>
+			<AppBar />
+			<RightPanel component="main">
+				<DrawerHeader />
+				<OutletWrapper>
+					<NoInternetPage>
+						<Outlet />
+					</NoInternetPage>
+				</OutletWrapper>
+			</RightPanel>
+		</LayoutComponent>
+	);
 };
 
 export default Layout;
