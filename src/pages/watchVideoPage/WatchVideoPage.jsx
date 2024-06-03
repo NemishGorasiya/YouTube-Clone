3db1c2667008249fa8705f24081a7c447b7adcb6
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-03 18:33:40 +0530

    replace loader with skeleton

--- src/pages/watchVideoPage/WatchVideoPage.jsx
+++ src/pages/watchVideoPage/WatchVideoPage.jsx
@@ -164,73 +164,64 @@ const WatchVideoPage = () => {
                 allow="fullscreen"
               />
             </YouTubeIframeWrapper>
-            {isLoading || isChannelDetailsLoading ? (
-              <Loader />
-            ) : (
-              <>
-                <h2>{title}</h2>
-                <VideoMetadataWrapper>
-                  <Stack
-                    direction="row"
-                    spacing={1.5}
-                    sx={{ alignItems: "center" }}
-                  >
-                    <ChannelLink to={`/channel/${channelId}`}>
-                      <Box
-                        component="img"
-                        sx={{
-                          height: 50,
-                          width: 50,
-                          borderRadius: "50%",
-                        }}
-                        alt="Channel Thumbnail"
-                        src={url}
-                        referrerPolicy="no-referrer"
-                      />
-                      <Stack>
-                        <Typography
-                          variant="h6"
-                          color="text.secondary"
-                          sx={{ display: "flex", alignItems: "center" }}
-                        >
-                          {channelTitle}
-                          <CheckCircleIcon
-                            fontSize="x-small"
-                            sx={{ marginLeft: "5px" }}
-                          />
-                        </Typography>
-                        <Typography variant="body1" color="text.secondary">
-                          {formatCompactNumber(subscriberCount)} subscribers
-                        </Typography>
-                      </Stack>
-                    </ChannelLink>
-                    <SubscribeButton channelId={channelId} />
-                  </Stack>
-                  <Stack direction="row" spacing={1.5}>
-                    <LikeDislike
-                      isLoggedIn={isLoggedIn}
-                      videoId={videoId}
-                      likeCount={likeCount}
-                      updateLikeCount={updateLikeCount}
-                    />
-                    <UserActionButton
-                      variant="contained"
-                      disabled={!isLoggedIn}
+            <h2>{title}</h2>
+            <VideoMetadataWrapper>
+              <Stack
+                direction="row"
+                spacing={1.5}
+                sx={{ alignItems: "center" }}
+              >
+                <ChannelLink to={`/channel/${channelId}`}>
+                  <Box
+                    component="img"
+                    sx={{
+                      height: 50,
+                      width: 50,
+                      borderRadius: "50%",
+                    }}
+                    alt="Channel Thumbnail"
+                    src={url}
+                    referrerPolicy="no-referrer"
+                  />
+                  <Stack>
+                    <Typography
+                      variant="h6"
+                      color="text.secondary"
+                      sx={{ display: "flex", alignItems: "center" }}
                     >
-                      <AddToPlaylist videoId={videoId} />
-                    </UserActionButton>
+                      {channelTitle}
+                      <CheckCircleIcon
+                        fontSize="x-small"
+                        sx={{ marginLeft: "5px" }}
+                      />
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      {formatCompactNumber(subscriberCount)} subscribers
+                    </Typography>
                   </Stack>
-                </VideoMetadataWrapper>
-                <VideoDescriptionContainer>
-                  <Typography variant="body1">
-                    {formatCompactNumber(viewCount)} views{" "}
-                    {calcDistanceToNow({ time: publishedAt })}{" "}
-                    {tags && tags.map((tag) => <Tag key={tag}>#{tag} </Tag>)}
-                  </Typography>
-                  <VideoDescription description={description} />
-                </VideoDescriptionContainer>
-              </>
-            )}
+                </ChannelLink>
+                <SubscribeButton channelId={channelId} />
+              </Stack>
+              <Stack direction="row" spacing={1.5}>
+                <LikeDislike
+                  isLoggedIn={isLoggedIn}
+                  videoId={videoId}
+                  likeCount={likeCount}
+                  updateLikeCount={updateLikeCount}
+                />
+                <UserActionButton variant="contained" disabled={!isLoggedIn}>
+                  <AddToPlaylist videoId={videoId} />
+                </UserActionButton>
+              </Stack>
+            </VideoMetadataWrapper>
+            <VideoDescriptionContainer>
+              <Typography variant="body1">
+                {formatCompactNumber(viewCount)} views{" "}
+                {calcDistanceToNow({ time: publishedAt })}{" "}
+                {tags && tags.map((tag) => <Tag key={tag}>#{tag} </Tag>)}
+              </Typography>
+              <VideoDescription description={description} />
+            </VideoDescriptionContainer>
           </VideoPlayerWrapper>
         )}
 
