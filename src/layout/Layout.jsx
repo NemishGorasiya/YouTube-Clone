8896118c9d51ca18ee0a1fd31510fca70fa9c81d
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-04 15:13:06 +0530

    scroll to top when path changes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,28 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 import AppBar from "../components/appBar/AppBar";
 import NoInternetPage from "../pages/noInternetPage/NoInternetPage";
 import {
-	DrawerHeader,
-	LayoutComponent,
-	OutletWrapper,
-	RightPanel,
+  DrawerHeader,
+  LayoutComponent,
+  OutletWrapper,
+  RightPanel,
 } from "./LayoutStyledComponent";
 
 const Layout = () => {
-	return (
-		<LayoutComponent>
-			<AppBar />
-			<RightPanel component="main">
-				<DrawerHeader />
-				<OutletWrapper>
-					<NoInternetPage>
-						<Outlet />
-					</NoInternetPage>
-				</OutletWrapper>
-			</RightPanel>
-		</LayoutComponent>
-	);
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <LayoutComponent>
+      <AppBar />
+      <RightPanel component="main">
+        <DrawerHeader />
+        <OutletWrapper>
+          <NoInternetPage>
+            <Outlet />
+          </NoInternetPage>
+        </OutletWrapper>
+      </RightPanel>
+    </LayoutComponent>
+  );
 };
 
 export default Layout;
