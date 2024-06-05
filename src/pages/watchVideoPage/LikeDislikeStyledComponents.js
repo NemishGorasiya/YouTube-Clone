5125c0f13268cb5f2bce9bae602a57e1352c64ec
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-05 15:30:43 +0530

    Applied uniform styling

--- src/pages/watchVideoPage/LikeDislikeStyledComponents.js
+++ src/pages/watchVideoPage/LikeDislikeStyledComponents.js
@@ -1,24 +1,30 @@
 import { Box, Button, styled } from "@mui/material";
 
 export const LikeDislikeButtonWrapper = styled(Box)(({ theme }) => ({
-	display: "flex",
-	alignItems: "center",
-	borderColor: "divider",
-	borderRadius: 24,
-	background: theme.palette.secondary.light,
+  display: "flex",
+  alignItems: "center",
+  borderColor: "divider",
+  borderRadius: 24,
+  background: theme.palette.secondaryBackground.default,
 }));
 
-export const LikeButton = styled(Button)(() => ({
-	borderTopRightRadius: 0,
-	borderBottomRightRadius: 0,
-	paddingLeft: 12,
-	display: "flex",
-	gap: 8,
+export const LikeButton = styled(Button)(({ theme }) => ({
+  borderTopRightRadius: 0,
+  borderBottomRightRadius: 0,
+  paddingLeft: 12,
+  display: "flex",
+  gap: 8,
+  "&:hover": {
+    background: theme.palette.secondaryBackground.light,
+  },
 }));
 
-export const DislikeButton = styled(Button)(() => ({
-	minWidth: "0",
-	paddingRight: 12,
-	borderTopLeftRadius: 0,
-	borderBottomLeftRadius: 0,
+export const DislikeButton = styled(Button)(({ theme }) => ({
+  minWidth: "0",
+  paddingRight: 12,
+  borderTopLeftRadius: 0,
+  borderBottomLeftRadius: 0,
+  "&:hover": {
+    background: theme.palette.secondaryBackground.light,
+  },
 }));
