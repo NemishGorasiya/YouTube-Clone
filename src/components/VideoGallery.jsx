17fcff548f15622e6377d73beb5d983ecea9a928
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-05 23:43:35 +0530

    fix: styling in video card

--- src/components/VideoGallery.jsx
+++ src/components/VideoGallery.jsx
@@ -7,91 +7,91 @@ import VideoCardSkeleton from "./VideoCardSkeleton";
 import { Grid } from "./VideoGalleryStyledComponents";
 
 const VideoGallery = ({
-  isListView = false,
-  url,
-  queryParams: queryParamsProp,
+	isListView = false,
+	url,
+	queryParams: queryParamsProp,
 }) => {
-  const [videos, setVideos] = useState({
-    list: [],
-    isLoading: true,
-    nextPageToken: "",
-  });
+	const [videos, setVideos] = useState({
+		list: [],
+		isLoading: true,
+		nextPageToken: "",
+	});
 
-  const { list, isLoading, nextPageToken } = videos;
+	const { list, isLoading, nextPageToken } = videos;
 
-  const fetchData = useCallback(
-    async ({ nextPageToken, abortController } = {}) => {
-      try {
-        setVideos((prevVideos) => ({
-          ...prevVideos,
-          isLoading: true,
-        }));
-        const queryParams = {
-          ...queryParamsProp,
-          pageToken: nextPageToken,
-          maxResults: 20,
-        };
-        const response = await httpRequest({
-          url: url,
-          abortController,
-          queryParams,
-        });
-        if (response) {
-          const { items, nextPageToken } = response;
-          setVideos((prevVideos) => ({
-            list: [...prevVideos.list, ...items],
-            isLoading: false,
-            nextPageToken,
-          }));
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    [queryParamsProp, url]
-  );
+	const fetchData = useCallback(
+		async ({ nextPageToken, abortController } = {}) => {
+			try {
+				setVideos((prevVideos) => ({
+					...prevVideos,
+					isLoading: true,
+				}));
+				const queryParams = {
+					...queryParamsProp,
+					pageToken: nextPageToken,
+					maxResults: 20,
+				};
+				const response = await httpRequest({
+					url: url,
+					abortController,
+					queryParams,
+				});
+				if (response) {
+					const { items, nextPageToken } = response;
+					setVideos((prevVideos) => ({
+						list: [...prevVideos.list, ...items],
+						isLoading: false,
+						nextPageToken,
+					}));
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		},
+		[queryParamsProp, url]
+	);
 
-  const loadMore = () => {
-    if (nextPageToken) {
-      fetchData({ nextPageToken });
-    }
-  };
+	const loadMore = () => {
+		if (nextPageToken) {
+			fetchData({ nextPageToken });
+		}
+	};
 
-  useEffect(() => {
-    setVideos({
-      list: [],
-      isLoading: true,
-      nextPageToken: "",
-    });
-    const abortController = new AbortController();
-    fetchData({ abortController: abortController });
-    return () => {
-      abortController.abort();
-    };
-  }, [fetchData]);
+	useEffect(() => {
+		setVideos({
+			list: [],
+			isLoading: true,
+			nextPageToken: "",
+		});
+		const abortController = new AbortController();
+		fetchData({ abortController: abortController });
+		return () => {
+			abortController.abort();
+		};
+	}, [fetchData]);
 
-  const renderItem = (video) => (
-    <VideoCard key={video.id} video={video} isListView={isListView} />
-  );
+	const renderItem = (video) => (
+		<VideoCard key={video.id} video={video} isListView={isListView} />
+	);
 
-  return (
-    <Grid container isListView={isListView}>
-      <InfiniteScroll
-        items={list}
-        fetchMoreData={loadMore}
-        renderItem={renderItem}
-        isLoading={isLoading}
-        skeletonItem={<VideoCardSkeleton isListView={isListView} />}
-        numberOfSkeletonItems={15}
-      />
-    </Grid>
-  );
+	return (
+		<Grid container isListView={isListView}>
+			<InfiniteScroll
+				items={list}
+				fetchMoreData={loadMore}
+				renderItem={renderItem}
+				isLoading={isLoading}
+				skeletonItem={<VideoCardSkeleton isListView={isListView} />}
+				numberOfSkeletonItems={15}
+			/>
+		</Grid>
+	);
 };
 
 VideoGallery.propTypes = {
-  isListView: PropTypes.bool,
-  url: PropTypes.string,
-  queryParams: PropTypes.object,
+	isListView: PropTypes.bool,
+	url: PropTypes.string,
+	queryParams: PropTypes.object,
 };
 
 export default VideoGallery;
