555b39bc73f07a4aa653f9971312d79568c02236
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-09 00:30:34 +0530

    Added no internet image

--- src/pages/noInternetPage/NoInternetPage.jsx
+++ src/pages/noInternetPage/NoInternetPage.jsx
@@ -1,52 +1,51 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import {
-  NoInternetFallbackPageContainer,
-  NoInternetImage,
-  NoInternetImageWrapper,
+	NoInternetFallbackPageContainer,
+	NoInternetImage,
+	NoInternetImageWrapper,
 } from "./NoInternetPageStyledComponents";
 import { Typography } from "@mui/material";
-import noInternetSvg from "./NoInternet.svg";
+import noInternetImage from "../../assets/noInternet.png";
+import { noInternetBase64Url } from "../../utils/constant";
 
 const NoInternetPage = ({ children }) => {
-  console.log("image", noInternetSvg);
-  const [isOnline, setOnline] = useState(navigator.onLine);
+	const [isOnline, setOnline] = useState(navigator.onLine);
 
-  useEffect(() => {
-    const handleOnline = () => setOnline(true);
-    const handleOffline = () => setOnline(false);
+	useEffect(() => {
+		const handleOnline = () => setOnline(true);
+		const handleOffline = () => setOnline(false);
 
-    window.addEventListener("online", handleOnline);
-    window.addEventListener("offline", handleOffline);
+		window.addEventListener("online", handleOnline);
+		window.addEventListener("offline", handleOffline);
 
-    // Clean up the event listeners on component unmount
-    return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
-    };
-  }, []);
+		// Clean up the event listeners on component unmount
+		return () => {
+			window.removeEventListener("online", handleOnline);
+			window.removeEventListener("offline", handleOffline);
+		};
+	}, []);
 
-  if (isOnline) {
-    return children;
-  } else {
-    return (
-      <NoInternetFallbackPageContainer>
-        <NoInternetImageWrapper>
-          <NoInternetImage
-            src={noInternetSvg}
-            // referrerPolicy="no-referrer"
-            alt="No Internet"
-          />
-        </NoInternetImageWrapper>
-        <Typography>Connect to the internet</Typography>
-        <Typography>You&apos;re offline. Check your connection.</Typography>
-      </NoInternetFallbackPageContainer>
-    );
-  }
+	if (isOnline) {
+		return children;
+	} else {
+		return (
+			<NoInternetFallbackPageContainer>
+				<NoInternetImageWrapper>
+					<NoInternetImage
+						src={`data:image/png;base64,${noInternetBase64Url}`}
+						alt="No Internet"
+					/>
+				</NoInternetImageWrapper>
+				<Typography>Connect to the internet</Typography>
+				<Typography>You&apos;re offline. Check your connection.</Typography>
+			</NoInternetFallbackPageContainer>
+		);
+	}
 };
 
 NoInternetPage.propTypes = {
-  children: PropTypes.node,
+	children: PropTypes.node,
 };
 
 export default NoInternetPage;
