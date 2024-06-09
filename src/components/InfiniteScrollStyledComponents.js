f440d654ec93a27c56bb0ec99562f0d792c80a58
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-09 16:04:13 +0530

    Fix: Bug in infinite scrolling

--- src/components/InfiniteScrollStyledComponents.js
+++ src/components/InfiniteScrollStyledComponents.js
@@ -2,7 +2,11 @@ import styledConfig from "../utils/styledConfig";
 import { Box } from "@mui/material";
 
 export const LoaderWrapper = styledConfig(Box)({
-  display: "flex",
-  justifyContent: "center",
-  gridColumn: "1/-1",
+	display: "flex",
+	justifyContent: "center",
+	gridColumn: "1/-1",
+});
+
+export const RenderItemWrapper = styledConfig(Box)({
+	height: "100%",
 });
