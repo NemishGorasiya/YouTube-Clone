5f934358bb9a66686940f27d4a938796b1c8f766
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-06-10 00:29:28 +0530

    replaced .scss with .css

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.scss";
+import "./index.css";
 import { ThemeContextProvider } from "./context/ThemeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeContextProvider>
+			<App />
+		</ThemeContextProvider>
+	</React.StrictMode>
 );
